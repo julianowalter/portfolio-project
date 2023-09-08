f564a48ad4ff032d6ffa590e6120c240cbb2cca3
julianowalter <julianows92@gmail.com>
2023-09-08 16:01:50 -0300

    Add content to landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,88 +1,31 @@
-import React, { useEffect, useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faLinkedin,
-  faMedium,
-  faStackOverflow,
-} from "@fortawesome/free-brands-svg-icons";
-import { Box, HStack } from "@chakra-ui/react";
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
 
-const socials = [
-  {
-    icon: faEnvelope,
-    url: "mailto: hello@example.com",
-  },
-  {
-    icon: faGithub,
-    url: "https://github.com",
-  },
-  {
-    icon: faLinkedin,
-    url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
-  },
-  {
-    icon: faStackOverflow,
-    url: "https://stackoverflow.com",
-  },
-];
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
 
-const Header = () => {
-  const handleClick = (anchor) => () => {
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={4}>
+      <VStack>
+      <Avatar src='https://i.pravatar.cc/150?img=7' size="2x1"/>
+      <Heading as="h4" size="md">{greeting}</Heading>
+      </VStack>
+      <VStack>
+        <Heading as="h1">{bio1}</Heading>
+        <Heading as="h1">{bio2}</Heading>
+      </VStack>
+    </VStack>
+  </FullScreenSection>
+);
 
-  return (
-    <Box
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <nav>
-            {/* Add social media links based on the `socials` data */}
-            <HStack spacing={10}>
-              {socials.map(({ icon, url}) => (
-                <a key={url} href={url} icon={icon} targets="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon key={url} icon={icon} size = "2x" />
-                </a>
-              ))}
-            </HStack>
-          </nav>
-          <nav>
-            <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
-                <a href="#projects" onClick={handleClick}>Projects</a>
-                <a href="#contact" onClick={handleClick}>Contact Me</a>
-            </HStack>
-          </nav>
-        </HStack>
-      </Box>
-    </Box>
-  );
-};
-export default Header;
+export default LandingSection;
