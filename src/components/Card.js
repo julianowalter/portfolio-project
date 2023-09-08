9650e91c9fd70d8b12f8a7dd5909c5a660c69fd7
julianowalter <julianows92@gmail.com>
2023-09-08 17:11:47 -0300

    Design card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,24 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack
+      color="black"
+      backgroundColor="white"
+      cursor="pointer"
+      borderRadius={6}
+    >
+      <Image src={imageSrc} alt={title} borderRadius={6}/>
+      <VStack spacing={3} alignItems="flex-start" p={5}>
+        <Heading as="h4" size="md">{title}</Heading>
+        <Text color="#808080">{description}</Text>
+        <HStack fontWeight="medium">
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
